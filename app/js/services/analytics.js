5ce7f460c60d402b4b6b8cb1ddd941ab622cd413
Christophe Hoch <christophe.hoch@propellerhead.co.nz>
2018-04-11 09:21:58 +1200

    Integrating firebase analytics

--- app/js/services/analytics.js
+++ app/js/services/analytics.js
@@ -11,26 +11,36 @@
                 if (typeof analytics !== 'undefined') {
                     window.analytics.startTrackerWithId('UA-54472828-1');
                 }
-            },
-            debugMode: function() {
-                if (typeof analytics !== 'undefined') {
-                    window.analytics.debugMode();
+
+                if (window.FirebasePlugin && typeof window.device !== 'undefined') {
+                    window.FirebasePlugin.setUserId(window.device.uuid);
                 }
             },
             screen: function(name) {
-                if (!name)
+                if (!name) {
                     return;
+                }
 
                 if (typeof analytics !== 'undefined') {
                     window.analytics.trackView(name);
                 }
+
+                if (window.FirebasePlugin) {
+                    window.FirebasePlugin.setScreenName(name);
+                }
             },
             event: function(category, action, label, value) {
-                if (!category || !action)
+                if (!category || !action) {
                     return;
+                }
 
                 if (typeof analytics !== 'undefined') {
                     window.analytics.trackEvent(category, action, label, value);
+                }
+
+                if (window.FirebasePlugin) {
+                    var params = value ? { 'item_id': value } : {};
+                    window.FirebasePlugin.logEvent(action, params);
                 }
             }
         };
